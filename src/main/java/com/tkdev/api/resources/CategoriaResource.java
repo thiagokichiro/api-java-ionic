b4c75f92c77b9f186c21bb8aca42a63ae49f6086
Thiago Kichiro <kichiro.88@gmail.com>
2021-07-28 17:30:31 -0300

    Testando primeira classe de domínio - Categoria

---
+++ src/main/java/com/tkdev/api/resources/CategoriaResource.java
@@ -0,0 +1,28 @@
+package com.tkdev.api.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tkdev.api.domain.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+
+		List<Categoria> listaCategorias = new ArrayList<>();
+		listaCategorias.add(cat1);
+		listaCategorias.add(cat2);
+
+		return listaCategorias;
+	}
+
+}
