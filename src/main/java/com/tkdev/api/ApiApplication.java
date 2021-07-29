e201fda95e63b1c79f499a435433bd88e4cfc201
Thiago Kichiro <kichiro.88@gmail.com>
2021-07-29 15:04:13 -0300

    Produto e associação muitos para muitos com Categoria

--- src/main/java/com/tkdev/api/ApiApplication.java
+++ src/main/java/com/tkdev/api/ApiApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.tkdev.api.domain.Categoria;
+import com.tkdev.api.domain.Produto;
 import com.tkdev.api.repositories.CategoriaRepository;
+import com.tkdev.api.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class ApiApplication implements CommandLineRunner{
-	
+public class ApiApplication implements CommandLineRunner {
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
@@ -22,11 +27,23 @@ public class ApiApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
+		Produto p1 = new Produto(null, "Computador", 4000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 60.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
