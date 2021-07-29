f5c94beae2c1a1b0e66a71afff75e1a0346974ba
Thiago Kichiro <kichiro.88@gmail.com>
2021-07-29 14:34:11 -0300

    Criando operação de instanciação

--- src/main/java/com/tkdev/api/ApiApplication.java
+++ src/main/java/com/tkdev/api/ApiApplication.java
@@ -1,13 +1,32 @@
 package com.tkdev.api;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.tkdev.api.domain.Categoria;
+import com.tkdev.api.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
