ff8af98428761833229a3b866f9e2a5d1f2a65bf
Thiago Kichiro <kichiro.88@gmail.com>
2021-08-22 19:33:01 -0300

    Atualizacao utilizando jsonignore

--- src/main/java/com/tkdev/api/domain/Categoria.java
+++ src/main/java/com/tkdev/api/domain/Categoria.java
@@ -10,8 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -29,7 +27,6 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 
-	@JsonManagedReference
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 
