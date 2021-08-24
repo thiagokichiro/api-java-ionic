89b59c459e1bb90e7af302348aad05db07f281c8
Thiago Kichiro <Thiago Kichiro>
2021-08-24 13:01:28 -0300

    Inserindo uma nova categoria

--- src/main/java/com/tkdev/api/resources/CategoriaResource.java
+++ src/main/java/com/tkdev/api/resources/CategoriaResource.java
@@ -3,9 +3,11 @@ package com.tkdev.api.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tkdev.api.domain.Categoria;
 import com.tkdev.api.services.CategoriaService;
@@ -21,6 +23,14 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria categoria = categoriaService.find(id);
 		return ResponseEntity.ok().body(categoria);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+		categoria = categoriaService.insert(categoria);
+		java.net.URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(categoria.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
